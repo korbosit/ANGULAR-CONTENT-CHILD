fd8727499a225b47c9587b6a3981aa4ce3354df5
Vlaimir Korba <krissky1986@gmail.com>
2024-02-16 14:33:13 +0200

    36-37 - ContentChild() ContentChildren() in Angular

--- src/app/parent/child/child.component.ts
+++ src/app/parent/child/child.component.ts
@@ -1,26 +1,35 @@
-import { Component, ContentChild, ElementRef, ContentChildren, QueryList } from '@angular/core';
-import { TestComponent } from 'src/app/test/test.component';
+import {
+    Component,
+    ContentChild,
+    ElementRef,
+    ContentChildren,
+    QueryList,
+} from "@angular/core";
+import { TestComponent } from "src/app/test/test.component";
 
 @Component({
-  selector: 'app-child',
-  templateUrl: './child.component.html',
-  styleUrls: ['./child.component.css']
+    selector: "app-child",
+    templateUrl: "./child.component.html",
+    styleUrls: ["./child.component.css"],
 })
 export class ChildComponent {
-  @ContentChild('para') paragraphEl: ElementRef;
+    constructor() {}
+    @ContentChild("para") paragraphEl: ElementRef;
 
-  @ContentChild(TestComponent) testEl: TestComponent;
+    @ContentChild(TestComponent) testEl: TestComponent;
 
-  @ContentChildren('para') paraElements: QueryList<ElementRef>;
+    @ContentChildren("para") paraElements: QueryList<ElementRef>;
 
-  @ContentChildren(TestComponent) testElements: QueryList<TestComponent>;
+    @ContentChildren(TestComponent) testElements: QueryList<TestComponent>;
 
-  StyleParagraph(){
-    // console.log(this.paragraphEl.nativeElement);
-    // console.log(this.testEl.name);
+    StyleParagraph() {
+        // console.log(this.paragraphEl.nativeElement);
+        // console.log(this.testEl.name);
 
-    //this.paraElements.forEach((el) => {console.log(el.nativeElement)});
+        //this.paraElements.forEach((el) => {console.log(el.nativeElement)});
 
-    this.testElements.forEach((el) => {console.log(el)});
-  }
+        this.testElements.forEach((el) => {
+            console.log(el);
+        });
+    }
 }
